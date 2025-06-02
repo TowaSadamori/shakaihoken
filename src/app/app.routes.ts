e4274991071e96753857b3fb75ff2312c7fb16e2
TowaSadamori <towa.sadamori@pathoslogos.co.jp>
2025-06-02 18:18:03 +0900

    レイアウト表示できた

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    // children: [
-    //   { path: '', component: HomeComponent },
-    // ],
+    children: [{ path: '', component: HomeComponent }],
   },
 ];
--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, RouterOutlet],
+  imports: [CommonModule, HeaderComponent, SidebarComponent, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
 })
