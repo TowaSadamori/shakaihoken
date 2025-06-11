b0c208b6ea28d81a59d0405c1ad0b2893ff6d38e
TowaSadamori <towa.sadamori@pathoslogos.co.jp>
2025-06-11 12:26:39 +0900

    戻るボタン実装

--- src/app/employee-salary-bonus/employee-salary-bonus-detail/employee-salary-bonus-detail.component.ts
+++ src/app/employee-salary-bonus/employee-salary-bonus-detail/employee-salary-bonus-detail.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-salary-bonus-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-salary-bonus-detail.component.html',
   styleUrl: './employee-salary-bonus-detail.component.scss',
 })
-export class EmployeeSalaryBonusDetailComponent {}
+export class EmployeeSalaryBonusDetailComponent {
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/employee-salary-bonus']);
+  }
+}
