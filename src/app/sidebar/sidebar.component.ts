a35473a9d5319a67768718980c75ef1e0c84298c
TowaSadamori <towa.sadamori@pathoslogos.co.jp>
2025-06-23 04:06:03 +0900

    微調整

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,10 +8,11 @@ import {
   SimpleChanges,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { AuthService } from '../services/auth.service';
 
 export interface NavItem {
   label: string;
@@ -35,8 +36,8 @@ export class SidebarComponent implements OnChanges {
     { label: 'ホーム', path: '/', icon: 'home' },
     { label: '従業員手続き', path: '/employee-procedures', icon: 'person' },
     { label: '給与賞与情報', path: '/employee-salary-bonus' },
-    { label: '被保険者の個人情報', path: '/insured-person-information' },
-    { label: 'その他申請', path: '/other-applications' },
+    { label: '被保険者の個人情報', path: '/employee-procedures/insured-person-form' },
+    { label: 'その他申請', path: '#' },
     { label: '事業所手続き', path: '/employer-procedures', icon: 'business' },
     { label: '事業所一覧', path: '/company-register' },
     { label: '設定', path: '/settings', icon: 'settings' },
@@ -45,6 +46,11 @@ export class SidebarComponent implements OnChanges {
 
   isLabelVisible = true;
   private widthTransitionMs = 200;
+
+  constructor(
+    public router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['isMini']) {
@@ -66,5 +72,13 @@ export class SidebarComponent implements OnChanges {
 
   goToCreateAccount() {
     window.location.href = '/create-account';
+  }
+
+  async goToOtherApplications() {
+    const currentUser = await this.authService.getCurrentUserProfileWithRole();
+    if (currentUser) {
+      this.router.navigate(['/employee-procedures/application-form', currentUser.uid]);
+    }
+    this.onNavItemClick();
   }
 }
